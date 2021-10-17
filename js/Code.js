b0fe06070d8d1ee350d7a6841d1c4085aec1608b
ManikoXXX <75440736+ManikoXXX@users.noreply.github.com>
2021-10-17 14:50:25 +0200

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "PROGRAMMING";
+const accessCode2 = "JAVASCRIPT";
+const accessCode3 = "CYBERSECURITY";
+
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R G O M A N I P M R", 100,50)
+    fill("lightblue")
+    text("Hint: Few People Have This As A Proffesion!", 33,72)
+
+    fill("white")
+    textSize(15)
+    text("A J T I C S R A P V", 700,150)
+    fill("lightblue")
+    text("Hint: It's A Language Used In Coding!", 636,172)
+
+    fill("white")
+    textSize(15)
+    text("Y C U B R I T E R C Y S E", 100,250)
+    fill("lightblue")
+    text("Hint: It Protects Us From Vulnerable Threats!", 33,272)
+
+}
